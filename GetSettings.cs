5cb72de9b6b645e6ad62f54367a1bd798e4d4dff
rummolprod999 <rummolprod999@gmail.com>
2017-05-16 11:02:21 +0300

    update

---
+++ GetSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace ParserContracts44
+{
+    public class GetSettings
+    {
+        public readonly string Database;
+        public readonly string TempdirContracts44;
+        public readonly string Logdir;
+        public readonly string Prefix;
+        public readonly string UserDB;
+        public readonly string PassDB;
+    }
+}
