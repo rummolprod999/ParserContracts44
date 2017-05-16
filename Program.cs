50c2ce971746c47a364d5a1d41819e93a6e5b743
rummolprod999 <rummolprod999@gmail.com>
2017-05-16 10:46:11 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParserContracts44
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
