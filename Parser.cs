d43fe2e9d014f8ea51a319733d110d8fae3d9560
rummolprod999 <rummolprod999@gmail.com>
2024-10-31 18:56:43 +0300

    fix

--- Parser.cs
+++ Parser.cs
@@ -64,26 +64,26 @@ namespace ParserContracts44
 
         public WorkWithFtp ClientFtp44_old()
         {
-            var ftpCl = new WorkWithFtp("ftp://ftp.zakupki.gov.ru", "free", "otkluchenie_FTP_s_01_01_2025_podrobnee_v_ATFF");
+            var ftpCl = new WorkWithFtp("ftp://ftp.zakupki.gov.ru", "free", "VNIMANIE!_otkluchenie_FTP_s_01_01_2025_podrobnee_v_ATFF");
             return ftpCl;
         }
         
         public WorkWithFtp ClientFtp223_old()
         {
-            var ftpCl = new WorkWithFtp("ftp://ftp.zakupki.gov.ru", "fz223free", "otkluchenie_FTP_s_01_01_2025_podrobnee_v_ATFF");
+            var ftpCl = new WorkWithFtp("ftp://ftp.zakupki.gov.ru", "fz223free", "VNIMANIE!_otkluchenie_FTP_s_01_01_2025_podrobnee_v_ATFF");
             return ftpCl;
         }
         
         public FtpClient ClientFtp44()
         {
-            var client = new FtpClient("ftp://ftp.zakupki.gov.ru", "free", "otkluchenie_FTP_s_01_01_2025_podrobnee_v_ATFF");
+            var client = new FtpClient("ftp://ftp.zakupki.gov.ru", "free", "VNIMANIE!_otkluchenie_FTP_s_01_01_2025_podrobnee_v_ATFF");
             client.Connect();
             return client;
         }
         
         public FtpClient ClientFtp223()
         {
-            var client = new FtpClient("ftp.zakupki.gov.ru", "fz223free", "otkluchenie_FTP_s_01_01_2025_podrobnee_v_ATFF");
+            var client = new FtpClient("ftp.zakupki.gov.ru", "fz223free", "VNIMANIE!_otkluchenie_FTP_s_01_01_2025_podrobnee_v_ATFF");
             client.Connect();
             return client;
         }
